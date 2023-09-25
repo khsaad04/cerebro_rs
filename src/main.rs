77e9ad10ff35f5f88c38767f0409f4bcba9e53f2
khsaad04 <khsaad0404@gmail.com>
2023-09-25 18:58:10 +0600

    nothing much

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use poise::serenity_prelude as serenity;
 use shuttle_poise::ShuttlePoise;
 use shuttle_secrets::SecretStore;
 
-pub struct Data {} // User data, which is stored and accessible in all command invocations
+pub struct Data {}
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, Data, Error>;
 
