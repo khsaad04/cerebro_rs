e9c53c185633e2042e51d732abd6907efe2bbe69
khsaad04 <khsaad0404@gmail.com>
2024-02-26 15:10:48 +0600

    chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,49 @@
 {
-  description = "devShell for ceremusic";
+  description = "cerebro devShell";
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
   outputs = {
-    self,
-    fenix,
-    flake-utils,
     nixpkgs,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default = let
-        overlays = [fenix.overlays.default];
-        pkgs = import nixpkgs {
-          inherit overlays;
-        };
-      in
-        with pkgs; {
-          devShells.default = mkShell {
-            nativeBuildInputs = [
-              pkg-config
-              gdb
-              (fenix.stable.withComponents [
-                "cargo"
-                "clippy"
-                "rust-src"
-                "rustc"
-                "rustfmt"
-              ])
-            ];
-          };
+    utils,
+    fenix,
+    ...
+  }: let
+    overlays = [fenix.overlays.default];
+
+    pkgsForSystem = system:
+      import nixpkgs {
+        inherit overlays system;
+        config.allowUnfree = true;
+      };
+
+    toolchain = "stable";
+  in
+    utils.lib.eachSystem ["x86_64-linux"] (system: let
+      legacyPackages = pkgsForSystem system;
+      rustPkg = legacyPackages.fenix."${toolchain}".withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+      ];
+    in rec {
+      inherit legacyPackages;
+
+      devShell = with legacyPackages;
+        mkShell {
+          buildInputs = [
+            gdb
+            rustPkg
+          ];
         };
     });
 }
